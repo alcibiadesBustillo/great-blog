652e8e84a7735edad9d1318caf3f37f37a7a1a7e
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-09-28 09:17:54 -0400

    Recommend post via email

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 # def post_list(request):
@@ -34,3 +36,25 @@ def post_detail(request, year, month, day, post):
         publish__day=day)
     
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    # retrive pos by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommend you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'bustillo.alcibiades@gmail.com', [cd['to']])
+            # ... send email
+            sent = True
+    else:
+        form = EmailPostForm()
+    
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent':sent})
--- core/settings.py
+++ core/settings.py
@@ -129,3 +129,10 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'username@gmail.com'
+EMAIL_HOST_PASSWORD = 'pass'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
